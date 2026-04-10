90d1d7c50cca5f0ff25da3761ee116b37f537bc1
Toginomi <rudyiipilande@gmail.com>
2026-04-10 12:03:54 +0800

    delete whole folder

--- team17_handcrafted_haven/src/app/layout_components/header/login.tsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link";
-import styles from "./header.module.css"
-
-export default function LoginButton() {
-    return (
-        <Link href='/login' className={styles.login}>Login</Link>
-    );
-}
--- team17_handcrafted_haven/src/app/shop/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function ShopPage() {
-  return <div>Shop Coming Soon</div>;
-}
